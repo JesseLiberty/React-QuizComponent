3c9a42933c4bf14b6577120ac69f99d7b3153c1b
Jesse Liberty <jesseliberty@gmail.com>
2019-10-29 14:11:22 -0400

    Quiz Component

---
+++ src/Quiz.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+let quizData = require('/quiz_data.json')
+
+class quizData extends Component {
+   state = {}
+   render() {
+      return (<div>Quiz</div>);
+   }
+}
+
+export default quizData;
